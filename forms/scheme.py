6c3127d9c3c6cefaa86e59f7fcdd4f5d74662a9f
qzane <qzane@live.com>
2015-05-10 21:30:40 +0800

    suggestions done

--- forms/scheme.py
+++ forms/scheme.py
@@ -62,3 +62,49 @@ class SearchMajorCourse(Form):
         '''
         
         return res
+        
+        
+class SearchMajorid(Form):
+    def __init__(self,r):
+        self.majorid=r.get('search_major')
+    def validate(self):
+        if self.majorid is not None:
+            return True
+        return False
+    def search(self):
+        res=[]#TM给跪了,直接Course.query就是没找到怎么写。。
+        #sbycode=Course.session.query(Course,func.count(Xk.stuid).label('sum')).outerjoin(Xk,Xk.code==Course.code).group_by(Course.code).filter(Course.code.like(self.scode+'%'))
+        
+        schemes=Majors.session.query(Majors).filter(Majors.majorname.like('%'+self.majorid+'%'))
+        sr1=schemes.all()
+        for i in sr1:
+            res.append(i)
+        schemes=Majors.session.query(Majors).filter(Majors.majorid.like('%'+self.majorid+'%'))
+        sr1=schemes.all()
+        for i in sr1:
+            res.append(i)
+        return res
+        '''
+        sr1.sort(key=lambda x:len(x.kindid))
+        for kind in sr1:
+            leftCredit = kind.credit
+            schemes = Coursekinds.session.query(Coursekinds,Kinds).filter(Coursekinds.kindid==kind.kindid).filter(Coursekinds.compulsory==1).outerjoin(Kinds,Kinds.kindid==Coursekinds.kindid)
+            sr=schemes.all()
+            for i in sr:
+                i.addi = len(res)
+                i.allcredit = kind.credit
+                res.append(i)
+                leftCredit -= i.Coursekinds.ccredir
+            
+            schemes = Coursekinds.session.query(Coursekinds,Kinds).filter(Coursekinds.kindid==kind.kindid).filter(Coursekinds.compulsory==0).outerjoin(Kinds,Kinds.kindid==Coursekinds.kindid)
+            schemes = schemes.order_by(func.random())
+            sr=schemes.all()
+            for i in sr:
+                if leftCredit <= 0:
+                    break
+                i.addi = len(res)
+                i.allcredit = kind.credit
+                res.append(i)
+                leftCredit -= i.Coursekinds.ccredir
+        return res
+        '''
--- views/stu.py
+++ views/stu.py
@@ -6,7 +6,7 @@ from flask import g, request, flash
 from flask import render_template, redirect, url_for,current_app
 from utils import login_user,require_stu,gen_course_table,get_credit
 from models import Student
-from forms import ProfileForm,SearchForm
+from forms import ProfileForm,SearchForm,SearchMajorCourse,SearchMajorid
 
 from models import Account
 __all__ = ['bp']
@@ -46,3 +46,30 @@ def xk():
         sres=form.search()
         return render_template('student/xk.html',result=sres)
     return render_template('student/xk.html')
+    
+
+@bp.route('/scheme/',methods=['GET','POST'])
+@require_stu
+def scheme():
+    if request.method == 'GET':
+        return render_template('scheme/recommend.html')
+    form = SearchMajorCourse(request.form)
+    #卧槽简直是坑啊，之前在这用wtform一切都没问题尼玛就是获取不到表单数据，索性SearchForm不继承Form，然后就过了，我勒个去，怀疑是wtform哪bug了
+    if form.validate():
+        sres=form.search()
+        return render_template('scheme/recommend.html',result=sres)
+    return render_template('scheme/recommend.html')
+
+
+@bp.route('/suggestions/',methods=['GET','POST'])
+@require_stu
+def majorname():
+    if request.method == 'GET':
+        return render_template('scheme/majorid.html')
+    form = SearchMajorid(request.form)
+    #卧槽简直是坑啊，之前在这用wtform一切都没问题尼玛就是获取不到表单数据，索性SearchForm不继承Form，然后就过了，我勒个去，怀疑是wtform哪bug了
+    if form.validate():
+        sres=form.search()
+        return render_template('scheme/majorid.html',result=sres)
+    return render_template('scheme/majorid.html')
+
